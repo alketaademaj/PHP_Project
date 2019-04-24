d0490b5272f748d08d01930f9303dbae8f4fa58b
Ramppu <46961429+Ramppu@users.noreply.github.com>
2019-04-24 13:28:18 +0300

    Update Config.php

--- PHPSQL/Config.php
+++ PHPSQL/Config.php
@@ -1,8 +1,20 @@
 <?php
-	$servername = "localhost";
-	$username = "root";
-	$password = "";
-	$dbname = "database";
+// Fetching server data
+$servername = '';
+$dbname = 'database';
+$username = '';
+$password = '';
+ 
+foreach ($_SERVER as $key => $value) {
+  if (strpos($key, "MYSQLCONNSTR_localdb") !== 0) {
+  continue;
+  }
+ 
+  $servername = preg_replace("/^.*Data Source=(.+?);.*$/", "\\1", $value);
+  $dbname = preg_replace("/^.*Database=(.+?);.*$/", "\\1", $value);
+  $username = preg_replace("/^.*User Id=(.+?);.*$/", "\\1", $value);
+  $password = preg_replace("/^.*Password=(.+?)$/", "\\1", $value);
+} 
 
 	// Create connection
 	$conn = new mysqli($servername, $username, $password,$dbname);
